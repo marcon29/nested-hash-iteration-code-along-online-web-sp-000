b047287dd1ea4da2f8d0801721fcddf59e65dd24
Mark Stabler <mark@markstabler.com>
2020-03-10 01:37:30 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,22 +14,15 @@ require 'pry'
   #   }
   # }
 
-# def remove_strawberry(contacts)
-#   contacts.each do |person, data|
-#     if person == "Freddy Mercury"
-#       data.each do |attribute, value|
-#         if attribute == :favorite_ice_cream_flavors
-#           value.delete_if{|flavor| flavor == "strawberry"}
-#         end
-#       end
-#     end
-#   end
-# end
-
 def remove_strawberry(contacts)
-  contacts["Freddy Mercury"].each do |attribute, value|
-    if attribute == :favorite_ice_cream_flavors
-      value.delete_if{|flavor| flavor == "strawberry"}
-    end
-  end
+ contacts.each do |person, data|
+   if person == "Freddy Mercury"
+     data.each do |attribute, value|
+       if attribute == :favorite_ice_cream_flavors
+         value.delete_if{|flavor| flavor == "strawberry"}
+       end
+     end
+   end
+ end
 end
+
